5f5892e9d2b41f2351e8ef6d524dae784991dd4e
Brian Gorrie <brgorrie@gmail.com>
2023-02-22 23:25:03 +0800

    add check for existance of git executable

--- Scripts/GitPull-AllSubdirectories.ps1
+++ Scripts/GitPull-AllSubdirectories.ps1
@@ -1,6 +1,11 @@
 # Enable strict mode
 Set-StrictMode -Version Latest
 
+# Check for the git executable
+if (!(Get-Command git -ErrorAction SilentlyContinue)) {
+    throw "Git executable not found. Please make sure Git is installed and in the system's path."
+}
+
 # Get the current directory
 $currentDirectory = Get-Location
 
@@ -11,18 +16,13 @@ $subDirectories = Get-ChildItem -Directory
 foreach ($subDirectory in $subDirectories) {
     try {
         # Change the current directory to the subdirectory
-        Set-Location $subDirectory.FullName
+        Set-Location "$($subDirectory.FullName)"
 
-		Write-Host
-		Write-Host "Checking if there is a repository to update at $subDirectory" 
+        Write-Host
+        Write-Host "Checking if there is a repository to update at $($subDirectory.FullName)" 
 
         # Check if the current directory is a git repository
         if (Test-Path -Path ".git" -PathType Container) {
-            # Check for the git executable
-            if (!(Get-Command git -ErrorAction SilentlyContinue)) {
-                throw "Git executable not found. Please make sure Git is installed and in the system's path."
-            }
-            
             # Execute git pull if it's a git repository
             git pull
         }
