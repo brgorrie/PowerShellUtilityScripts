b6eaf024e5f31706e2f42fbf9455bb41294f8825
Brian Gorrie <brgorrie@gmail.com>
2023-02-22 23:40:15 +0800

    catered for the remote not being configured - ie. new repositories I have yet to push to github

--- Scripts/GitPull-AllSubdirectories.ps1
+++ Scripts/GitPull-AllSubdirectories.ps1
@@ -23,8 +23,17 @@ foreach ($subDirectory in $subDirectories) {
 
         # Check if the current directory is a git repository
         if (Test-Path -Path ".git" -PathType Container) {
-            # Execute git pull if it's a git repository
-            git pull
+            # Check if there is a remote configured for the repository
+            $remote = (git remote -v)
+
+			if ($remote -ne $null -and $remote.Count -gt 0) {
+                Write-Host "Retrieving updates from $($remote)" 
+                # Execute git pull if there is a remote configured for the repository
+                git pull
+            }
+            else {
+                Write-Host "The repository at $subDirectory has no remote configured."
+            }
         }
     }
     catch {
