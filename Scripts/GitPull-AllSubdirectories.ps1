c6046ee225ef140ce104d1ce8dcf7547584b7752
Brian Gorrie <brgorrie@gmail.com>
2023-02-22 23:19:05 +0800

    improved the quality of the script and added additional output to make it easier to use.

--- Scripts/GitPull-AllSubdirectories.ps1
+++ Scripts/GitPull-AllSubdirectories.ps1
@@ -1,3 +1,6 @@
+# Enable strict mode
+Set-StrictMode -Version Latest
+
 # Get the current directory
 $currentDirectory = Get-Location
 
@@ -6,15 +9,29 @@ $subDirectories = Get-ChildItem -Directory
 
 # Loop through each subdirectory and execute git pull if it's a git repository
 foreach ($subDirectory in $subDirectories) {
-    # Change the current directory to the subdirectory
-    Set-Location $subDirectory.FullName
+    try {
+        # Change the current directory to the subdirectory
+        Set-Location $subDirectory.FullName
 
-    # Check if the current directory is a git repository
-    if (Test-Path -Path ".git" -PathType Container) {
-        # Execute git pull if it's a git repository
-        git pull
+		Write-Host
+		Write-Host "Checking if there is a repository to update at $subDirectory" 
+
+        # Check if the current directory is a git repository
+        if (Test-Path -Path ".git" -PathType Container) {
+            # Check for the git executable
+            if (!(Get-Command git -ErrorAction SilentlyContinue)) {
+                throw "Git executable not found. Please make sure Git is installed and in the system's path."
+            }
+            
+            # Execute git pull if it's a git repository
+            git pull
+        }
     }
-
-    # Change the current directory back to the original directory
-    Set-Location $currentDirectory
+    catch {
+        Write-Error $_.Exception.Message
+    }
+    finally {
+        # Change the current directory back to the original directory
+        Set-Location $currentDirectory
+    }
 }
