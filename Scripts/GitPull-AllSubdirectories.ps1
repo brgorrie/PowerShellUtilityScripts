f015885e5b52283b7d1e50056ab56ca69fc3b3bb
Brian Gorrie <brgorrie@gmail.com>
2023-02-22 23:47:36 +0800

    some tweaks to the comments and console output.  Script is complete for now.

--- Scripts/GitPull-AllSubdirectories.ps1
+++ Scripts/GitPull-AllSubdirectories.ps1
@@ -18,22 +18,25 @@ foreach ($subDirectory in $subDirectories) {
         # Change the current directory to the subdirectory
         Set-Location -LiteralPath $subDirectory.FullName
 
-        Write-Host
-        Write-Host "Checking if there is a repository to update at $($subDirectory.FullName)" 
-
-        # Check if the current directory is a git repository
+        # Check if there is a repository to update
+		Write-Host 
+        Write-Host "Checking if $($subDirectory.Name) is a Git repository" 
         if (Test-Path -LiteralPath ".git" -PathType Container) {
+
             # Check if there is a remote configured for the repository
             $remote = (git remote -v)
 
             if ($remote -ne $null -and $remote.Count -gt 0) {
-                Write-Host "Retrieving updates from $($remote)" 
+                Write-Host "Updating $($subDirectory.Name) from $($remote)" 
                 # Execute git pull if there is a remote configured for the repository
                 git pull
             }
             else {
-                Write-Host "The repository at $($subDirectory.FullName) has no remote configured."
+                Write-Host "The repository $($subDirectory.Name) has no remote configured."
             }
+        }
+        else {
+            Write-Host "$($subDirectory.Name) is not a Git repository."
         }
     }
     catch {
