f8e409b49401b700120001f66e49fbd3733e7755
Brian Gorrie <brgorrie@gmail.com>
2023-02-26 09:49:19 +0800

    updated readme and added paramaeter to script for output directory.

--- README.md
+++ README.md
@@ -15,17 +15,41 @@ Head to the jira for the project https://github.com/brgorrie/PowerShellUtilitySc
 
 ### GitPull-AllSubdirectories.ps1
 
+#### Description
 This script will iterate through all the subdirectories of the specified directory or if no parameter is supplied all the subdirectories of the current directory.  It will check if the directory is for a git repository, and that it has a remote configured.  If it does have a remote configured it will then do a git pull to update the local clone of the repository.
 
-Example useage with no path parameter:
+#### Parameter: path
+Optional parameter that specifies the path to the directory containing subdirectories to check. Defaults to the current directory if no parameters are specified.
+
+#### Example useage with no path parameter
 
 ```
    PS C:\> cd c:\git
    PS C:\git> .\PowerShellUtilityScripts\Scripts\GitPull-AllSubdirectories.ps1
 ```   
 
-Example useage with a path parameter:
+#### Example useage with a path parameter
 ```
    PS C:\git> cd .\PowerShellUtilityScripts\Scripts\
    PS C:\git\PowerShellUtilityScripts\Scripts> .\GitPull-AllSubdirectories.ps1 c:\git   
 ```
+
+### Concatenate-Files.ps1
+
+#### Description
+   This script takes an optional parameter which can specify a file extension, if one isn't specified then *.cs files are what is searched for.
+   The script searches for files with that extension in the current directory and all subdirectories concatenating them together into a *.txt file in the specified output directory.
+   The output file name is in the following format All[FileExtension]Files_YYYY-MM-DD_HH-mm-ss.txt for example AllCsFiles_2023-02-25_22-50-14.txt
+
+#### Parameter FileExtension
+   Optional parameter that specifies the file extension to search for and concatenate.  If not specified then the script defaults to *.cs files. 
+   
+#### Parameter OutputDirectory
+   Optional parameter that specifies the output directory for the concatenated file. If not specified then the default directory is C:\git.
+   
+#### Example useage to concatenate all *.cs files in a repo
+```
+   PS C:\> cd c:\git\DiceRoller
+   PS C:\git\DiceRoller> .\PowerShellUtilityScripts\Scripts\Concetenate-Files.ps1 -FileExtension cs -OutputDirectory C:\output
+```   
+	
--- Scripts/Concatenate-Files.ps1
+++ Scripts/Concatenate-Files.ps1
@@ -1,20 +1,21 @@
 <#
-
    .SYNOPSIS
-   Takes a file extension as a parameter, finds all files with that extension in the current directory and subdirectories, and then combines them all into a single text file in the c:\git directory. 
+   Takes a file extension as a parameter, finds all files with that extension in the current directory and subdirectories, and then combines them all into a single text file.
 
    .DESCRIPTION
-
    This script takes an optional parameter which can specify a file extension, if one isn't specified then *.cs files are what is searched for.
-   The script searches for files with that extension in the current directory and all subdirectories concatenating them together into a *.txt file in the c:\git directory.
+   The script searches for files with that extension in the current directory and all subdirectories concatenating them together into a *.txt file in the specified output directory.
    The output file name is in the following format All[FileExtension]Files_YYYY-MM-DD_HH-mm-ss.txt for example AllCsFiles_2023-02-25_22-50-14.txt
    
    Example useage to concatenate all *.cs files in a repo:
    PS C:\> cd c:\git\DiceRoller
-   PS C:\git\DiceRoller> .\PowerShellUtilityScripts\Scripts\Concetenate-Files.ps1 -FileExtension cs
+   PS C:\git\DiceRoller> .\PowerShellUtilityScripts\Scripts\Concetenate-Files.ps1 -FileExtension cs -OutputDirectory C:\output
    
    .PARAMETER FileExtension
    Optional parameter that specifies the file extension to search for and concatenate.  If not specified then the script defaults to *.cs files. 
+   
+   .PARAMETER OutputDirectory
+   Optional parameter that specifies the output directory for the concatenated file. If not specified then the default directory is C:\git.
    
    .NOTES
    Author: Brian Gorrie 
@@ -36,16 +37,17 @@
 #>
 
 param (
-    [string]$FileExtension = "cs"
+    [string]$FileExtension = "cs",
+    [string]$OutputDirectory = "C:\git"
 )
 
 # Define variables for the output file and Git executable path
 $outputFileName = "All${FileExtension^}Files_" + (Get-Date -Format "yyyy-MM-dd_HH-mm-ss") + ".txt"
-$outputFilePath = Join-Path -Path "C:\git" -ChildPath $outputFileName
+$outputFilePath = Join-Path -Path $OutputDirectory -ChildPath $outputFileName
 
 # Check if the output directory exists and is a folder
-if (!(Test-Path -Path "C:\git" -PathType Container)) {
-    Write-Error "Output directory C:\git does not exist or is not a folder."
+if (!(Test-Path -Path $OutputDirectory -PathType Container)) {
+    Write-Error "Output directory $OutputDirectory does not exist or is not a folder."
     return
 }
 
