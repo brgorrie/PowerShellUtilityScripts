77d68eca729289716102bbbde3d0db159b6448a0
Brian Gorrie <brgorrie@gmail.com>
2023-03-05 17:17:05 +0800

    more cleanup of warnings

--- Scripts/GitPull-AllSubdirectories.ps1
+++ Scripts/GitPull-AllSubdirectories.ps1
@@ -64,8 +64,8 @@ foreach ($subDirectory in $subDirectories) {
         Set-Location -LiteralPath $subDirectory.FullName
 
         # Check if there is a repository to update
-		Write-Output 
-        Write-Output "Checking if $($subDirectory.Name) is a Git repository" 
+		Write-Output
+        Write-Output "Checking if $($subDirectory.Name) is a Git repository"
         if (Test-Path -LiteralPath ".git" -PathType Container) {
 
             # Check if there is a remote configured for the repository
